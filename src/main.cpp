7261c5ffc4b9aed2526af2109f2571a2b21f0a39
sophivlk <sophialoukili21@gmail.com>
2026-01-27 14:50:11 +0100

    ajout main.cpp qui appelle jeu

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,18 @@
+#include "Jeu.hpp"
 #include <iostream>
 
 int main() {
-    std::cout << "=== TP6 - Jeu Stratégique ===" << std::endl;
-    std::cout << "Bienvenue dans le jeu!" << std::endl;
-    
-    std::cout << "Ca se lance bien !" << std::endl;
-    
+    std::cout << "=== BIENVENUE DANS OTRIO ===" << std::endl;
+
+    // 1. Création de l'instance du jeu
+    Jeu partie;
+
+    // 2. Initialisation (Création des joueurs, du plateau...)
+    partie.initialiser();
+
+    // 3. Lancement de la boucle principale
+    partie.lancerPartie();
+
+    std::cout << "Merci d'avoir joue !" << std::endl;
     return 0;
 }

262d659f559c308c72b4f3d935a9e378651c347b
sophivlk <sophialoukili21@gmail.com>
2026-02-01 16:02:53 +0100

    machine.hpp : ajout méthode privée estGagnant

--- lib/Machine.hpp
+++ lib/Machine.hpp
@@ -2,6 +2,7 @@
 
 #include <utility>
 #include <vector>
+#include <string>
 
 #include "Case.hpp"
 #include "Joueur.hpp"
@@ -11,10 +12,13 @@ class Machine : public Joueur {
 public:
   Machine(const std::string& nom, Couleur couleur);
 
+  // Méthode principale
+  std::pair<std::pair<int, int>, Cercle> deciderAction(Plateau &plateau) override;
+
+  // Méthode de défense
   std::pair<std::pair<int, int>, Cercle> testerMenaceImminente(const Plateau &plateau);
 
-  std::pair<std::pair<int, int>, Cercle>  deciderAction(Plateau &plateau);
-
+  // Méthodes de Pattern Matching 
   std::pair<std::pair<int, int>, Cercle> testerAlignementTailleCroissante(
         const Case& c1, const Case& c2, const Case& c3,
         int x1, int y1, int x2, int y2, int x3, int y3);
@@ -24,4 +28,8 @@ public:
         int x1, int y1, int x2, int y2, int x3, int y3);
 
   Taille analyserLigne(const Case& c1, const Case& c2, const Case& cible);
+
+private:
+  // Aide pour l'attaque en simulatioj
+  bool estGagnant(const Plateau& p, Couleur c);
 };
